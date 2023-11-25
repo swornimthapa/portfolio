2128ce70dd030278ca80a22027d1699f75c67a58
swornimthapa <swornimtheking@gmail.com>
2023-11-25 13:27:47 +0545

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let squareCaretdown = document.querySelector(".caretDown");
+squareCaretdown.onclick=function(){
+    let navbar = document.querySelector(".navbar .navbar-box ul");
+    navbar.classList.toggle("active");
+}
