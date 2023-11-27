7bf158aa6ce1d373b81474716c86f862e61252ec
swornimthapa <swornimtheking@gmail.com>
2023-11-27 22:51:04 +0545

    navbar scroll fixed

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ squareCaretdown.onclick=function(){
     let navbar = document.querySelector(".navbar .navbar-box ul");
     navbar.classList.toggle("active");
 }
+
+projectContainers.forEach(container => {
+    container.addEventListener('mouseenter', () => {
+      container.classList.add('zoomed');
+    });
+  
+    container.addEventListener('mouseleave', () => {
+      container.classList.remove('zoomed');
+    });
+  });
+
+
+
