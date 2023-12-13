b12c3116ac66051bbf038569b04354d939f2210b
swornimthapa <swornimtheking@gmail.com>
2023-12-13 21:41:26 +0545

    updated animations in home page

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,15 @@
 // Check viewport width and execute animation only if it meets certain conditions
 function executeAnimation() {
-    // curtainReveal();
+  // curtainReveal();
   if (window.innerWidth >= 1070) {
-        executeInitialAnimation();
-    }
-    shakeMouse();
-    
-
+    executeInitialAnimation();
+  }
+  executeSecondaryAnimations();
+  shakeMouse();
 }
-function executeInitialAnimation() {
-  let t1 = gsap.timeline();
 
+function executeInitialAnimation() {
+  let t1 = gsap.timeline(); 
   t1.from(".logo .active", {
     y: -150,
     duration: 0.5,
@@ -32,63 +31,59 @@ function executeInitialAnimation() {
     opacity: 0,
   });
 
-  let t2= gsap.timeline();
- t2.to(".line",{
-    height: "100%",
-    duration:1,
-    delay:1,
- })
-
-  t2.to(".intro-container .start .curtain-reveal",{
-    width :0,
-    duration:0.5,
-    delay:0.2,
-    ease: Circ.easeOutIn,
-  })
-
-  t2.to(".intro-container  .curtain-reveal",{
-    width :0,
-    duration:1,
-    // delay:0.2,
-    ease: Circ.easeOutIn,
-    stagger:0.2,
-  })
-  
-
   
 }
 
-
-
-function shakeMouse(){
-    let t2 = gsap.timeline({ repeat: -1 });
-    t2.to(".mouse", {
-      duration: 0.3,
-      x: -5, // Move 5 pixels to the left
-      ease: "power1.inOut",
+function executeSecondaryAnimations(){
+    let t2 = gsap.timeline();
+    t2.to(".line", {
+      height: "100%",
+      duration: 1,
+      delay: 1,
     });
-    t2.to(".mouse", {
-      duration: 0.3,
-      x: 5, // Move 5 pixels to the right
-      ease: "power1.inOut",
+  
+    t2.to(".intro-container .start .curtain-reveal", {
+      width: 0,
+      duration: 0.5,
+      delay: 0.2,
+      ease: Circ.easeOutIn,
     });
-    t2.to(".mouse", {
-      duration: 0.3,
-      x: 0, // Move back to the original position
-      ease: "power1.inOut",
+  
+    t2.to(".intro-container  .curtain-reveal", {
+      width: 0,
+      duration: 1,
+      // delay:0.2,
+      ease: Circ.easeOutIn,
+      stagger: 0.2,
     });
 }
 
-function curtainReveal(){
-    gsap.to(".curtain-reveal",{
-        width :0,
-        duration:1,
-        delay:1,
-        ease: Circ.easeOutIn,
-    })
+function shakeMouse() {
+  let t2 = gsap.timeline({ repeat: -1 });
+  t2.to(".mouse", {
+    duration: 0.3,
+    x: -5, // Move 5 pixels to the left
+    ease: "power1.inOut",
+  });
+  t2.to(".mouse", {
+    duration: 0.3,
+    x: 5, // Move 5 pixels to the right
+    ease: "power1.inOut",
+  });
+  t2.to(".mouse", {
+    duration: 0.3,
+    x: 0, // Move back to the original position
+    ease: "power1.inOut",
+  });
 }
 
+function curtainReveal() {
+  gsap.to(".curtain-reveal", {
+    width: 0,
+    duration: 1,
+    delay: 1,
+    ease: Circ.easeOutIn,
+  });
+}
 
 executeAnimation();
-
-
